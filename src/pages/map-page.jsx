0b322fcb32100a0fb90923a2ecc59ba851c5de17
ryusemin <tpals3799@naver.com>
2024-04-27 01:28:13 +0900

    🐛 🐛 🐛

--- src/pages/map-page.jsx
+++ src/pages/map-page.jsx
@@ -48,8 +48,8 @@ function MapPage() {
     //     level: 3 // 지도의 확대 레벨
     // };
 
-    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-    var map = new kakao.maps.Map(mapContainer, mapOption); 
+    // // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    // var map = new kakao.maps.Map(mapContainer, mapOption); 
 
 
     const mapDiv = document.getElementById('map_div');
