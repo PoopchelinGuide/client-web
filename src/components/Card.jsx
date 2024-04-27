7db14dcdb7969ce3473655679d592e63df140fd4
ryusemin <tpals3799@naver.com>
2024-04-27 22:46:04 +0900

    ✅ 팝업, 현재위치로 정보 불러오기 구현

---
+++ src/components/Card.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Card.css";
+
+function Card({
+  title,
+  subTitle,
+  content,
+  content2,
+  content3,
+  mainImage,
+  id,
+  price,
+  count,
+}) {
+  return (
+    <div className="card">
+      <div>
+        {/* <div style={{ display: "flex", alignItems: "center" }}> */}
+        <h1 className="card-title">{title}</h1>
+        <span className="card-sub-title">{subTitle}</span>
+        {/* </div> */}
+        <p className="card-content">{content}</p>
+        <p className="card-content2"> {content2}</p>
+        <p className="card-price">
+          {price ? "가격: " : null}
+          {price} {price ? "₩" : null}
+        </p>
+        {/* <p className="card-count">
+          {count ? "재고: " : null} {count}
+        </p> */}
+        <p className="card-content3">{content3}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/map-page.jsx
+++ src/pages/map-page.jsx
@@ -1,26 +1,145 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import '../styles/map-style.css';
+import { useNavigate } from 'react-router-dom';
 import Navigatorbar from '../components/navigatorbar';
+
+// import Card from "../components/Card"; // Card 컴포넌트 임포트
+import "../components/Card.css"; // Card 컴포넌트의 CSS 파일 임포트
+import { Card } from "antd";
+
 
 const { Tmapv2 } = window;
 const { kakao } = window;
 
 function MapPage() {
+  const navigate = useNavigate();
+
   var map;
   var marker_s, marker_e, marker_p1, marker_p2;
+
+  var markers = []; // 마커를 담을 배열
+	var markerList = [], // 마커 정보를 담은 배열
+  selectedMarker = null; // 클릭한 마커를 담을 변수
+  let prevInfo = null; // 이전에 열린 팝업 정보를 저장하는 변수
+
+
   var polyline_ = null; // 현재 폴리라인을 저장할 변수
+  let [popupInfo, setPopupInfo] = useState(null); // 현재 열려있는 팝업 정보를 저장하는 변수, boolean
+
+
+  var imageSize = new kakao.maps.Size(42, 56); // 마커의 크기 기존 33, 36
+  var choiceImageSize = new kakao.maps.Size(44, 58); // 선택한 마커의 크기 기존 38, 40
+  
+
 
   var totalMarkerArr = [];
 	var drawInfoArr = [];
 	var resultdrawArr = [];
 
+	// fetch 통신 method
+	const fetchData = async (BodyJson, latlng, initMarkers) => {
+	try {
+		const response = await fetch(
+		`${process.env.NODE_ENV === "development" ? "http://" : ""}${
+			process.env.REACT_APP_API_URL
+		}store/read/map/coordinate`,
+		{
+			method: "POST",
+			headers: {
+			"Content-type": "application/json",
+			},
+			body: BodyJson,
+		}
+		);
+		if (response.status === 200) {
+		const { data } = await response.json();
+		// 서버에서 받은 데이터를 markerList에 저장
+		markerList = data;
+		console.log("데이터 전송 완료");
+		console.log(markerList);
+		// message.success(`주변에 상점이 ${markerList.length}개 존재합니다.`, 2);
+		initMarkers();
+		} else if (response.status === 400) {
+		// message.error("발견된 상점이 존재하지 않습니다.", 2);
+		console.log("데이터 전송 실패");
+		}
+	} catch (error) {
+		// message.error("잘못된 요청입니다.");
+		console.error("오류 발생:", error);
+	}
+	};
+
+
+
+
+
+	//Popup창 켜고 끄는 method
+	function showPopup(info) {
+
+	console.log("팝업창을 띄웁니다.");
+	// 현재 열린 팝업 정보가 null이 아니고, 새로운 팝업이 이전 팝업과 같다면 팝업을 닫고 함수를 종료합니다.
+	if (prevInfo !== null && prevInfo === info) {
+		prevInfo = null;
+		setPopupInfo(prevInfo);
+		return;
+	}
+	
+		// 그렇지 않은 경우, 즉 현재 열린 팝업이 없거나 새로운 팝업이 이전 팝업과 다르다면
+		// 현재 열린 팝업 정보를 새로운 팝업 정보로 업데이트합니다
+		prevInfo = info;
+		setPopupInfo(prevInfo);
+	  }
+
+	  function initMarkers() {
+        if (markerList === null) {
+          console.log("데이터가 없습니다.");
+          return;
+        }
+        markerList.forEach(function (markerInfo) {
+          // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+              position: new kakao.maps.LatLng(
+                markerInfo.storeLocationY,
+                markerInfo.storeLocationX
+              ),
+              map: map,
+              image: normalImage, // 마커 이미지
+            });
+            marker.normalImage = normalImage;
+            marker.clickImage = clickImage;
+          
+          markers.push(marker);
+
+          // 마커에 click 이벤트를 등록합니다
+          kakao.maps.event.addListener(marker, "click", function () {
+            // 클릭된 마커가 없거나, click 마커가 클릭된 마커가 아니면
+            // 마커의 이미지를 클릭 이미지로 변경합니다
+
+            if (!selectedMarker || selectedMarker !== marker) {
+              // 클릭된 마커 객체가 null이 아니면
+              // 클릭된 마커의 이미지를 기본 이미지로 변경하고
+              !!selectedMarker &&
+                selectedMarker.setImage(selectedMarker.normalImage);
+
+              // 현재 클릭된 마커의 이미지는 클릭 이미지로 변경합니다
+              marker.setImage(marker.clickImage);
+              // 클릭된 마커를 현재 클릭된 마커 객체로 설정합니다
+              selectedMarker = marker;
+            } else if (selectedMarker === marker) {
+              selectedMarker.setImage(selectedMarker.normalImage);
+              selectedMarker = null;
+            }
+            showPopup("a");
+          });
+        });
+      }
+
 
 
 // 보행자 경로 그리기 함수
 function drawLine(arrPoint) {
-	
-
 
     var points = [];
     arrPoint.forEach((element) => {
@@ -59,42 +178,124 @@ function drawLine(arrPoint) {
 	if (!mapContainer.firstChild) {
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
     map = new kakao.maps.Map(mapContainer, mapOption); 
-	}
+	
+      // 원을 생성합니다
+      var circle = new kakao.maps.Circle({
+        center: locPosition,
+        radius: 10000,
+        strokeWeight: 5, // 선의 두께입니다
+        strokeColor: "#75B8FA", // 선의 색깔입니다
+        strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        strokeStyle: "dashed", // 선의 스타일 입니다
+        fillOpacity: 0, // 채우기 불투명도 입니다
+      });
+
+      var centerAround = circle.getBounds();
+	  circle.setMap(map); // 원을 지도에 표시합니다
+      console.log("원 생성"+centerAround);
+
+      // centerAround의 남서쪽과 북동쪽 좌표를 가져옵니다
+      var swLatLng = centerAround.getSouthWest();
+      var neLatLng = centerAround.getNorthEast();
+
+      var circleXY = {
+        minX: swLatLng.getLng(), // 남서쪽 경도
+        minY: swLatLng.getLat(), // 남서쪽 위도
+        maxX: neLatLng.getLng(), // 북동쪽 경도
+        maxY: neLatLng.getLat(), // 북동쪽 위도
+      };
+
+	  // fetchData(JSON.stringify(circleXY), options.center, initMarkers);
+	  var prevLatlng; // 이전 중심 좌표를 저장할 변수
+
+		// 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+		kakao.maps.event.addListener(map, "dragend", function () {
+			// 지도의  레벨을 얻어옵니다
+			var level = map.getLevel();
+			// 지도의 중심좌표를 얻어옵니다
+			var latlng = map.getCenter();
+	
+			circle.setPosition(latlng); // 지도의 중심좌표를 원의 중심으로 설정합니다
+			circle.setRadius(level * 1000); // 원의 반지름을 지도의 레벨 * 1000으로 설정합니다
+			circle.setMap(map); // 원을 지도에 표시합니다
+	
+			// 이전 중심 좌표가 있고, 새로운 중심 좌표와의 차이가 0.1 미만이면 AJAX 요청을 보내지 않습니다
+			if (
+				prevLatlng &&
+				Math.abs(prevLatlng.getLat() - latlng.getLat()) < 0.01 &&
+				Math.abs(prevLatlng.getLng() - latlng.getLng()) < 0.01
+			) {
+				return;
+			}
+			// 새로운 중심 좌표를 이전 중심 좌표로 저장합니다
+			prevLatlng = latlng;
+	
+			var centerAround = circle.getBounds();
+			console.log(centerAround);
+	
+			swLatLng = centerAround.getSouthWest();
+			neLatLng = centerAround.getNorthEast();
+	
+			circleXY = {
+				minX: swLatLng.getLng(),
+				minY: swLatLng.getLat(),
+				maxX: neLatLng.getLng(),
+				maxY: neLatLng.getLat(),
+			};
+	
+			console.log(circleXY);
+	
+			for (var i = 0; i < markers.length; i++) {
+				markers[i].setMap(null);
+			}
+	
+			// markers 배열 초기화
+			markers = [];
+	
+			var BodyJson = JSON.stringify(circleXY);
+			// fetchData(BodyJson, latlng, initMarkers);
+			});
 
 
-    // const mapDiv = document.getElementById('map');
-
-    // // map_div가 이미 존재하면 초기화하지 않음
-    // if (!mapDiv.firstChild) {
-    //   map2 = new Tmapv2.Map('map_div', {
-    //     center: new window.Tmapv2.LatLng(
-    //       37.566481622437934,
-    //       126.98502302169841
-    //     ),
-    //     zoom: 17,
-    //   });
-    // }
-
+}
   }
 
 function routeNavigation(locPosition, lat, lon){
     // 2. 시작, 도착 심볼찍기
 
-    marker_s = new kakao.maps.Marker(
-		{
-		  position : locPosition,
-		  // icon : "/upload/tmap/marker/pin_r_m_s.png",
-		  iconSize : new kakao.maps.Size(24, 38),
-		  map : map
-		});
+    // marker_s = new kakao.maps.Marker(
+	// 	{
+	// 	  position : locPosition,
+	// 	  // icon : "/upload/tmap/marker/pin_r_m_s.png",
+	// 	  iconSize : new kakao.maps.Size(24, 38),
+	// 	  map : map
+	// 	});
+
+		console.log("현재 위치 "+ lat + " " + lon);
   
 	  // 도착
 	  marker_e = new kakao.maps.Marker(
 		{
-		  position : new kakao.maps.LatLng(37.53822193303926,127.12627274769703),
+		  position : new kakao.maps.LatLng(35.8678658,128.5967954),
 		  // icon : "/upload/tmap/marker/pin_r_m_e.png",
 		  iconSize : new kakao.maps.Size(24, 38),
 		  map : map
+		});
+
+		// 마커에 click 이벤트를 등록합니다
+		kakao.maps.event.addListener(marker_e, "click", function () {
+		// 클릭된 마커가 없거나, click 마커가 클릭된 마커가 아니면
+		// 마커의 이미지를 클릭 이미지로 변경합니다
+
+		if (!selectedMarker || selectedMarker !== marker_s) {
+			// 클릭된 마커 객체가 null이 아니면
+
+			// 클릭된 마커를 현재 클릭된 마커 객체로 설정합니다
+			selectedMarker = marker_e;
+		} else if (selectedMarker === marker_e) {
+			selectedMarker = null;
+		}
+		showPopup("b");
 		});
 
 		var headers = {}; 
@@ -243,7 +444,6 @@ function asd(){
 		  console.log("현재위치를 가져올 수 없습니다.");
 		}
 	  });
-
 	      // 위치 정보를 가져온 후에 지도를 초기화하는 함수
 		  getLocation.then((locPosition) => {
 			initKakaoMap(locPosition);
@@ -266,7 +466,9 @@ function asd(){
 				  marker_s = new kakao.maps.Marker({
 					map: map,
 					position: locPosition,
+					iconSize : new kakao.maps.Size(24, 38),
 				  });
+
 				  routeNavigation(locPosition, lat, lon);
 
 				  console.log("사용자의 위치를 지속적으로 추적합니다.");
@@ -296,6 +498,20 @@ function asd(){
       <div
         id="map_div"
       ></div>
+
+	        {/* 팝업 정보가 있을 때만 Card 컴포넌트 렌더링 */}
+
+			{popupInfo && (
+        <div className="map-store-data" style={{ zIndex: 100 }}>
+		<Card
+			className='antCard'
+      		title="보성 화장실"
+      		extra={<a href="#" onClick={(e) => { e.preventDefault(); navigate('/review') }}>상세 리뷰</a>}>
+      <p>휴지가 없어요 태그</p>
+      <p>깨끗하다 </p>
+    </Card>
+        </div>
+      )}
       {Navigatorbar()}
     </>
   );
